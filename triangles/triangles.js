ec1266a5cb5f74ecaaf7d01fd974b20a83e09919
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-08-09 14:40:33 +0200

    Added rgb-hsl color conversion functions

--- triangles/triangles.js
+++ triangles/triangles.js
@@ -113,6 +113,100 @@ const getRatio = (start, end, target) => (
   (target - start) / (end - start)
 )
 
+const hueToRgb = (p, q, t) => {
+  if (t < 0) {
+    t += 1
+  }
+  if (t > 1) {
+    t -= 1
+  }
+  if (t < 1/6) {
+    return p + (q - p) * 6 * t
+  }
+  if (t < 1/2) {
+    return q
+  }
+  if (t < 2/3) {
+    return p + (q - p) * (2/3 - t) * 6
+  }
+  return p
+}
+
+const hslToRgb = ({h, s, l}) => {
+  let r, g, b
+
+  if (s == 0) {
+    r = g = b = l
+  } else {
+    const q = (l < 0.5) ? (l * (1 + s)) : (l + s - (l * s))
+    const p = (2 * l) - q
+    r = hueToRgb(p, q, h + 1/3)
+    g = hueToRgb(p, q, h)
+    b = hueToRgb(p, q, h - 1/3)
+  }
+
+  return {
+    r: r * 255,
+    g: g * 255,
+    b: b * 255
+  }
+}
+
+const rgbToHsl = ({r, g, b}) => {
+  r /= 255
+  g /= 255
+  b /= 255
+  const max = Math.max(r, g, b)
+  const min = Math.min(r, g, b)
+  let h, s, l = (max + min) / 2
+
+  if (max == min) {
+    h = s = 0
+  } else {
+    const d = max - min
+    s = (l > 0.5) ? (d / (2 - max - min)) : (d / (max + min))
+
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0)
+        break
+      case g:
+        h = (b - r) / d + 2
+        break
+      case b:
+        h = (r - g) / d + 4
+        break
+    }
+
+    h /= 6
+  }
+
+  return {h, s, l}
+}
+
+const adjustColor = ({color, adjustments}) => {
+  const {h, s, l} = rgbToHsl(color)
+  const adjustedHslColor = {
+    h: clamp({
+      min: 0,
+      max: 360,
+      value: h * (adjustments.hue + 1)
+    }),
+    s: clamp({
+      min: 0,
+      max: 1,
+      value: s * (adjustments.saturation + 1)
+    }),
+    l: clamp({
+      min: 0,
+      max: 1,
+      value: l * (adjustments.lightness + 1)
+    })
+  }
+  return hslToRgb(adjustedHslColor)
+}
+
+// TODO: Clean this up
 const getTriangleColor = ({
   triangle,
   colorFuzz,
