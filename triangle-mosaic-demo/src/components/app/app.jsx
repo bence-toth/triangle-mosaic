a50b1c38b168f475159b2e667e0900327f61623f
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-08-15 18:41:01 +0200

    Added to-do

--- triangle-mosaic-demo/src/components/app/app.jsx
+++ triangle-mosaic-demo/src/components/app/app.jsx
@@ -10,6 +10,7 @@
 // TODO: ESLint
 // TODO: Add tests (Jest, Cypress)
 // TODO: Update favicons, manifest, html
+// TODO: GitHub pages: https://medium.com/mobile-web-dev/how-to-build-and-deploy-a-react-app-to-github-pages-in-less-than-5-minutes-d6c4ffd30f14
 
 import React, {useState, useReducer, useEffect, useRef} from 'react'
 
