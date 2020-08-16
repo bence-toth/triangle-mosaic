d21f78b16e17a99c2ff95538015445394f0a5da4
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2020-08-17 00:13:00 +0200

    Updates

---
+++ precache-manifest.32dd9dbd1a37e2077764c14ec812e913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9231ef03a30dca952c5dc322bf01636",
+    "url": "/triangle-mosaic/index.html"
+  },
+  {
+    "revision": "38ae1ec78b19d1e5e500",
+    "url": "/triangle-mosaic/static/css/main.4a1fc938.chunk.css"
+  },
+  {
+    "revision": "c291489c81552b544524",
+    "url": "/triangle-mosaic/static/js/2.b3cb2206.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/triangle-mosaic/static/js/2.b3cb2206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ae1ec78b19d1e5e500",
+    "url": "/triangle-mosaic/static/js/main.07fb26ae.chunk.js"
+  },
+  {
+    "revision": "85f268b74e8095c29834",
+    "url": "/triangle-mosaic/static/js/runtime-main.77b8f31c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14920539b164c96bd136661784bf3f1a.js"
+  "/triangle-mosaic/precache-manifest.32dd9dbd1a37e2077764c14ec812e913.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/triangle-mosaic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
